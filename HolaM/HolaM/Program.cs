8ffe62060a8ced184f91be9af529f8bacb54b475
DianaVegaFierro <17340318@itnogales.edu.mx>
2021-12-03 09:52:38 -0700

    NuevoPrograma
    Agrego programa de prueba

---
+++ HolaM/HolaM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
